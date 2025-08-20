4982464d032d16f386debb1eb0e515a7618bb8fc
joão pedro cruz <joaocruzzdev@gmail.com>
2025-08-20 10:45:52 -0300

    add: claimUsernameForm component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,34 +5,38 @@ import Image from "next/image";
 import previewImage from "../../assets/mainIMG.png";
 import heroBackground from "../../assets/mainBG.png"
 
+import { ClaimUsernameForm } from "./components/claim-username-form";
+
 export default function Home() {
-   return (
-      <Container>
-         <Hero>
-         <Heading size="4xl">
-            Agendamento descomplicado
-         </Heading>
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">
+          Agendamento descomplicado
+        </Heading>
 
-         <Text size="xl">
-            Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre.
-         </Text>
-         </Hero>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre.
+        </Text>
 
-         <Image
-         src={heroBackground}
-         alt="Grid pontilhado"
-         height={682} style={{ position: "absolute", marginLeft: -238 }}
-         />
+        <ClaimUsernameForm />
+      </Hero>
 
-         <Preview>
-         <Image
-            priority
-            height={400}
-            quality={100}
-            src={previewImage}
-            alt="Calendário simbolizando aplicação em funcionamento"
-         />
-         </Preview>
-      </Container>
+      <Image
+        src={heroBackground}
+        alt="Grid pontilhado"
+        height={682} style={{ position: "absolute", marginLeft: -238, zIndex: -1 }}
+      />
+
+      <Preview>
+        <Image
+          priority
+          height={400}
+          quality={100}
+          src={previewImage}
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
   )
 }
