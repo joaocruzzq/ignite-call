46b1226775a0b5357326a42e728a3c21244e8bba
joão pedro cruz <joaocruzzdev@gmail.com>
2025-08-20 10:36:34 -0300

    add: home page structure

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,38 @@
+import { Heading, Text } from "@ignite-ui/react";
+import { Container, Hero, Preview } from "./styles";
+
+import Image from "next/image";
+import previewImage from "../../assets/mainIMG.png";
+import heroBackground from "../../assets/mainBG.png"
+
+export default function Home() {
+   return (
+      <Container>
+         <Hero>
+         <Heading size="4xl">
+            Agendamento descomplicado
+         </Heading>
+
+         <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre.
+         </Text>
+         </Hero>
+
+         <Image
+         src={heroBackground}
+         alt="Grid pontilhado"
+         height={682} style={{ position: "absolute", marginLeft: -238 }}
+         />
+
+         <Preview>
+         <Image
+            priority
+            height={400}
+            quality={100}
+            src={previewImage}
+            alt="Calendário simbolizando aplicação em funcionamento"
+         />
+         </Preview>
+      </Container>
+  )
+}
