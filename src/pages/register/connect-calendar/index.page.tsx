1fa70de71936118f00285facef307494d01dd6f4
joão pedro cruz <joaocruzzdev@gmail.com>
2025-08-26 15:42:59 -0300

    add: error message and adjustments

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,15 +1,22 @@
 import { Container, Header } from "../styles";
-import { ConnectBox, ConnectItem } from "./styles";
+import { AuthError, ConnectBox, ConnectItem } from "./styles";
 
-import { ArrowRight } from "phosphor-react";
+import { ArrowRight, Check } from "phosphor-react";
 import { Button, Heading, MultiStep, Text } from "@ignite-ui/react";
 
+import { useRouter } from "next/router";
 import { signIn, useSession } from "next-auth/react";
 
 export default function Register() {
+   const router = useRouter()
    const session = useSession()
-   
-   // async function handleRegister() {}
+
+   const hasAuthError = !!router.query.error
+   const isSignedIn = session.status === "authenticated"
+
+   async function handleConnectCalendar() {
+      await signIn("google")
+   }
 
    return (
       <Container>
@@ -31,13 +38,28 @@ export default function Register() {
                   Google Calendar
                </Text>
 
-               <Button variant={"secondary"} size={"sm"} onClick={() => signIn("google")}>
-                  Conectar
-                  <ArrowRight />
-               </Button>
+               {
+                  isSignedIn ? (
+                     <Button size={"sm"} disabled>
+                        Conectado
+                        <Check />
+                     </Button>
+                  ) : (
+                     <Button variant={"secondary"} size={"sm"} onClick={handleConnectCalendar}>
+                        Conectar
+                        <ArrowRight />
+                     </Button>
+                  )
+               }
             </ConnectItem>
 
-            <Button type="submit">
+            {hasAuthError && (
+               <AuthError size={"sm"}>
+                  Falha ao se conectar ao Google, verifique se você habilitou as permissões de acesso ao Google Calendar.
+               </AuthError>
+            )}
+
+            <Button type="submit" disabled={!isSignedIn}>
                Próximo passo
                <ArrowRight />
             </Button>
--- src/pages/register/connect-calendar/styles.ts
+++ src/pages/register/connect-calendar/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from "@ignite-ui/react";
+import { Box, styled, Text } from "@ignite-ui/react";
 
 export const ConnectBox = styled(Box, {
    marginTop: "$6",
@@ -13,8 +13,13 @@ export const ConnectItem = styled("div", {
    justifyContent: "space-between",
 
    padding: "$4 $6",
-   marginBottom: "$2",
+   marginBottom: "$4",
 
    borderRadius: "$md",
    border: "1px solid $gray600",
+})
+
+export const AuthError = styled(Text, {
+   color: "#F75A68",
+   marginBottom: "$4"
 })
